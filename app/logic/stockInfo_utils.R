8533d6903c4c711846047eb8f70488ac004d3c0c
IvanSortino97 <ivan.sortino97@gmail.com>
2024-07-19 01:00:53 +0200

    Scrape info

--- app/logic/stockInfo_utils.R
+++ app/logic/stockInfo_utils.R
@@ -3,7 +3,7 @@ box::use(
   #spsComps[addLoader],
   shiny[getShinyOption, shinyOptions],
   lubridate[ years, `%m-%`],
-  rvest[html_node,html_table, read_html]
+  rvest[html_nodes, html_text, read_html, html_table, ]
 )
 
 #' @export
@@ -43,7 +43,7 @@ get_sp500 <- function(){
 
     webpage <- read_html(url)
     sp500_table <- webpage |>
-      html_node(xpath = '//*[@id="constituents"]') |>
+      html_nodes(xpath = '//*[@id="constituents"]') |>
       html_table(fill = TRUE)
 
     # colnames(sp500_table) <- c(
@@ -89,6 +89,58 @@ years_ago <- function(years) {
   date <- Sys.Date()
   date_x_years_ago <- date %m-% years(years)
   return(date_x_years_ago)
+}
+
+#' @export
+#' Scraper for yahoo finance infos
+scrape_yahoo_finance <- function(ticker) {
+  # Define the URL for Yahoo Finance
+  url <- paste0("https://finance.yahoo.com/quote/", ticker)
+  webpage <- read_html(url)
+
+  # Define the XPath expressions for scraping
+  scrapelist <- list(
+    description = '//p[@class="yf-1xu2f9r" and not(@title)]',
+    stockPrice = '//*[@data-field="regularMarketOpen"]',
+    fiftyTwoWeekRange = '//*[@data-field="fiftyTwoWeekRange"]',
+    averageVolume  = '//*[@data-field="averageVolume"]',
+    peRatioTTM = '//li[span[contains(text(), "PE Ratio (TTM)")]]//fin-streamer[@data-field="trailingPE"]/@data-value',
+    epsTTM = '//li[span[contains(text(), "EPS (TTM)")]]//fin-streamer[@data-field="trailingEps"]/@data-value',
+    earningsDate = '//li[.//span[contains(text(),"Earnings Date")]]//span[contains(@class, "value")]',
+    marketCap = '//li[p[contains(text(), "Market Cap")]]/p[@class="value yf-1n4vnw8"]',
+    enterpriseValue = '//li[p[contains(text(), "Enterprise Value ")]]/p[@class="value yf-1n4vnw8"]',
+    trailingPE = '//li[p[contains(text(), "Trailing P/E ")]]/p[@class="value yf-1n4vnw8"]',
+    forwardPE = '//li[p[contains(text(), "Forward P/E ")]]/p[@class="value yf-1n4vnw8"]',
+    pegRatio = '//li[p[contains(text(), "PEG Ratio (5yr expected) ")]]/p[@class="value yf-1n4vnw8"]',
+    priceSale = '//li[p[contains(text(), "Price/Sales  (ttm)")]]/p[@class="value yf-1n4vnw8"]',
+    priceBook = '//li[p[contains(text(), "Price/Book  (mrq)")]]/p[@class="value yf-1n4vnw8"]',
+    enterpriseValueRevenue = '//li[p[contains(text(), "Enterprise Value/Revenue ")]]/p[@class="value yf-1n4vnw8"]',
+    enterpriseValueEBITDA = '//li[p[contains(text(), "Enterprise Value/EBITDA ")]]/p[@class="value yf-1n4vnw8"]',
+    profitMargin = '//li[p[contains(text(), "Profit Margin")]]/p[@class="value yf-lc8fp0"]',
+    returnOnAssets = '//li[p[contains(text(), "Return on Assets  (ttm)")]]/p[@class="value yf-lc8fp0"]',
+    returnOnEquity = '//li[p[contains(text(), "Return on Equity  (ttm)")]]/p[@class="value yf-lc8fp0"]',
+    revenue = '//li[p[contains(text(), "Revenue  (ttm)")]]/p[@class="value yf-lc8fp0"]',
+    netIncome = '//li[p[contains(text(), "Net Income Avi to Common  (ttm)")]]/p[@class="value yf-lc8fp0"]',
+    dilutedEPS = '//li[p[contains(text(), "Diluted EPS  (ttm)")]]/p[@class="value yf-lc8fp0"]',
+    totalCash = '//li[p[contains(text(), "Total Cash  (mrq)")]]/p[@class="value yf-lc8fp0"]',
+    totalDebtEquity = '//li[p[contains(text(), "Total Debt/Equity  (mrq)")]]/p[@class="value yf-lc8fp0"]',
+    leveredFreeCashFlow = '//li[p[contains(text(), "Levered Free Cash Flow  (ttm)")]]/p[@class="value yf-lc8fp0"]'
+  )
+
+  # Function to extract and clean the data
+  extract_data <- function(xpath) {
+
+    data <- webpage |>
+      html_nodes(xpath = xpath) |>
+      html_text()
+    data <- gsub(",", "", data)
+    if (length(data) == 0) {
+      return(NA)
+    }
+    return(data[1])
+  }
+
+  return(lapply(scrapelist, extract_data))
 }
 
 
--- app/view/stockInfo.R
+++ app/view/stockInfo.R
@@ -1,13 +1,13 @@
 box::use(shiny[div, moduleServer, NS, selectizeInput,reactiveVal, ...],
          bslib[page_fillable, card, card_header, card_body],
          shinyWidgets[switchInput, updateSwitchInput, prettyCheckbox],
-         echarts4r[echarts4rOutput, renderEcharts4r, e_legend,e_title,e_tooltip,e_line,e_charts],
+         echarts4r[..., echarts4rOutput, renderEcharts4r, e_legend,e_title,e_tooltip,e_line,e_charts],
          dplyr[filter],
          zoo[coredata, index],
          data.table[data.table],
          quantmod[getSymbols],
-         reactable[reactableOutput, renderReactable, reactable,colDef, getReactableState],)
-box::use(app / logic / stockInfo_utils[get_symbols,get_sp500, make_list, years_ago])
+         reactable[reactableOutput, renderReactable, reactable,colDef,reactableTheme, getReactableState],)
+box::use(app / logic / stockInfo_utils[get_symbols,get_sp500, make_list, years_ago, scrape_yahoo_finance])
 
 
 #' @export
@@ -65,6 +65,10 @@ ui <- function(id) {
     card(
       card_header("Data"),
       card_body(echarts4rOutput(ns("stockPlot")))
+    ),
+    card(
+      card_header("Scraped Info"),
+      card_body(verbatimTextOutput(ns("scrapedInfo")))
     )
 
   )
@@ -73,103 +77,124 @@ ui <- function(id) {
 
 #' @export
 server <- function(id, ...) {
+
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    symbols_dt <- get_symbols()
     sp500 <- get_sp500()$Symbol
-    symbol_list <- make_list(symbols_dt)
-    selectedStock <- reactiveVal(NULL)
+    symbols_dt <- reactive({
+      if (input$sp500Switch)
+        filter(get_symbols(), Symbol %in% sp500)
+      else
+        get_symbols()
+    })
+
+    selectedTicker <- reactiveVal("")
+    selectedStockInfo <- reactiveVal(NULL)
     data <- reactiveVal(NULL)
+    data_xts <- reactiveVal(NULL)
+    scraped_info <- reactiveVal(NULL)
 
-    filtered_symbols <- reactive({
-      if (input$sp500Switch)  filter(symbols_dt, Symbol %in% sp500) else symbols_dt
-    })
+    observe(
+      updateSelectizeInput(
+        inputId = "selectStock",
+        choices = make_list(symbols_dt()),
+        selected = "",
+        server = TRUE
+      )
+    )
 
-    updateSelectizeInput(inputId = "selectStock",
-                         choices = symbol_list,
-                         selected = "",
-                         server = TRUE)
-
-    output$stockTable <- renderReactable({
-
-      filtered_symbols() |> reactable(compact = TRUE,
-                                      showPageInfo = FALSE,
-                                      searchable = TRUE,
-                                      highlight = TRUE,
-                                      wrap = FALSE,
-                                      selection = "single",
-                                      onClick = "select",
-                                      columns = list(
-                                        Symbol = colDef(sticky = "left",
-                                                        width = 80),
-                                        Name = colDef(width = 350),
-                                        Financial.Status = colDef(width = 150),
-                                        ETF = colDef(width = 80)
-                                      ))
-    })
-
-
-
-    # Pull data when stock is selected
     observeEvent(input$selectStock, {
-
       req(input$selectStock)
-
+      selectedTicker(input$selectStock)
       updateSwitchInput(inputId = "tableSwitch", value = F)
 
-      ticker <- input$selectStock
-      stock_data <- getSymbols(Symbols = ticker,
-                               src = "yahoo",
-                               auto.assign = FALSE,
-                               from = years_ago(10) )
+    })
 
-      stock_data <- data.table(date = index(stock_data),
-                               coredata(stock_data))
-
-      names(stock_data) <- sub(paste0(input$selectStock, "."),"", names(stock_data))
-
-      data(stock_data)
-      selectedStock(filtered_symbols() |> filter(Symbol == ticker))
-
+    output$stockTable <- renderReactable({
+      symbols_dt() |> reactable(
+        compact = TRUE,
+        showPageInfo = FALSE,
+        searchable = TRUE,
+        highlight = TRUE,
+        wrap = FALSE,
+        selection = "single",
+        onClick = "select",
+        columns = list(
+          Symbol = colDef(sticky = "left",
+                          width = 80),
+          Name = colDef(width = 350),
+          Financial.Status = colDef(width = 150),
+          ETF = colDef(width = 80)
+        ),
+        theme = reactableTheme(searchInputStyle = list(width = "100%"))
+      )
     })
 
     # Update selected stock from table list
     observeEvent(getReactableState("stockTable", "selected"), {
-      selected <- getReactableState("stockTable", "selected")
-      if (length(selected) > 0) {
-        selected_stock <- filtered_symbols()[selected, 1]
-        updateSelectizeInput(inputId = "selectStock",
-                             selected = selected_stock)
-      }
+      selected_stock <- symbols_dt()[getReactableState("stockTable", "selected"), 1]
+      updateSelectizeInput(inputId = "selectStock",
+                           selected = selected_stock)
+      selectedTicker(selected_stock)
+      #updateSwitchInput(inputId = "tableSwitch", value = F)
     })
 
+
+
+
+    # Pull data when stock is selected
+    observeEvent(selectedTicker(), {
+      req(selectedTicker())
+
+      stock_data <- getSymbols(
+        Symbols = selectedTicker(),
+        src = "yahoo",
+        auto.assign = FALSE,
+        from = years_ago(10)
+      )
+      data_xts(stock_data)
+      scraped_info(scrape_yahoo_finance(selectedTicker()))
+
+      stock_data <- data.table(date = as.character(index(stock_data)),
+                                coredata(stock_data))
+
+      names(stock_data) <-
+        sub(paste0(selectedTicker(), "."), "", names(stock_data))
+
+      data(stock_data)
+      selectedStockInfo(symbols_dt() |> filter(Symbol == selectedTicker()))
+
+    })
+
+
     output$stockTitle <- renderText({
-      req(selectedStock())
-      selectedStock()$Name
+      req(selectedStockInfo())
+      selectedStockInfo()$Name
     })
 
     output$stockTicker <- renderText({
-      req(selectedStock())
-      selectedStock()$Symbol
+      req(selectedStockInfo())
+      selectedStockInfo()$Symbol
     })
 
     output$stockPlot <- renderEcharts4r({
-
       req(data())
 
       data() |>
         e_charts(date) |>
-        e_line(Close,
-               name = selectedStock()$Symbol,
-               smooth = TRUE,
-               symbol = "none",
-               animation = TRUE) |>
-        e_title("10-Year Historical Stock Data") |>
+        e_candle(opening = Open, closing = Close, low = Low, high = High, name = selectedStockInfo()$Symbol) |>
+        e_datazoom(type = "slider") |>
+        e_title(selectedStockInfo()$Name, "Quantmod data") |>
         e_tooltip(trigger = "axis") |>
         e_legend()
     })
 
+
+    output$scrapedInfo <- renderText({
+      scraped_info()$description
+
+      })
 
 
   })
